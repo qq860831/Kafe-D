ad815f50a5ce607d8330cba8283dceb650305030
qq860831 <qq860831@gmail.com>
2020-07-31 17:13:15 +0800

    123

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
-$(Kafe D).ready(function() {
+$(window).ready(function() {
     if($(window).scrollTop()>0)
         $(".navbar").removeClass("navbar-top");
     else
         $(".navbar").addClass("navbar-top");
   });
+
+//   skroller
+  var s = skrollr.init();  
