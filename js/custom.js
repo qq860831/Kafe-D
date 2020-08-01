3d94b8d7f6a6523b04410fb764f8cce1ae7a286c
qq860831 <qq860831@gmail.com>
2020-08-01 22:57:31 +0800

    123

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,18 @@
-$(window).ready(function() {
-    if($(window).scrollTop()>0)
-        $(".navbar").removeClass("navbar-top");
-    else
-        $(".navbar").addClass("navbar-top");
-  });
+$(window).ready(function () {
+  $(window).on("scroll", function () {
+    var scrollDistance = $(window).scrollTop();
+    var $header = $(".js-header");
+    if (scrollDistance > 500) {  //80剛好就是離開上面有人物的地方
+      $header.addClass("header--colored");
+    }
+    else {
+      $header.removeClass("header--colored");
+    }
+  })
 
-//   skroller
-  var s = skrollr.init();  
+ 
+
+})
+
+//   skroller begin
+var s = skrollr.init(); 
